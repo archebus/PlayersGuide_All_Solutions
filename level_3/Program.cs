d05b7bc2501d9f3d31bbbda8ba6f63890b634449
archebus <joshua.devon@gmail.com>
2025-03-21 19:01:46 +1100

    more level 3 stuff

--- level_3/Program.cs
+++ level_3/Program.cs
@@ -1,6 +1,19 @@
-﻿int i = 0;
+﻿void speakLots() {
 
-while (i < 5) {
-	Console.WriteLine($"Hey I've said this {i} times");
-	i++;
+	int i = 0;
+	
+	while (i < 5) {
+		Console.WriteLine($"Hey I've said this {i} times");
+		i++;
+	}
 }
+
+void whoseBread() {
+	Console.Write("Bread is ready!\nWho is the bread for?\n");
+	string? name = Console.ReadLine();
+	Console.Write($"Noted: {name} got some bread!\n");
+}
+
+whoseBread();
+
+// speakLots();
--- level_3/obj/Debug/net8.0/C#_Players_Guide.AssemblyInfo.cs
+++ level_3/obj/Debug/net8.0/C#_Players_Guide.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#_Players_Guide")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6700921bf52a857f3242068fdcf2adf0b84d7795")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#_Players_Guide")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#_Players_Guide")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
