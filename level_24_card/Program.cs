c5b62da750bf4a03f2f8e1de6e64fcdb45d86096
archebus <joshua.devon@gmail.com>
2025-03-24 19:15:17 +1100

    More challenges from level24 done

---
+++ level_24_card/Program.cs
@@ -0,0 +1,49 @@
+﻿// The Card
+// - define enumurations for card color and card ranks.
+// - colors = red, green, blue, yellow
+// - ranks = 1 - 10 then $, %, ^, & (face cards.)
+// - define class card to represent using the enums.
+// - add props or methods to tell you if card is a number or a symbol.
+// - main method will create a card instance for the whole deck and display all.
+
+enum Color {red,green,blue,yellow}
+enum Rank {one,two,three,four,five,six,seven,eight,nine,ten,dollar,percent,caret,ampersand}
+
+class Card
+{
+	public Color Color { get; }
+	public Rank Rank { get; }
+
+	public Card(Color col, Rank rank) 
+	{
+		Color = col;
+		Rank = rank;
+	}
+
+	public override string ToString() => $"{Color} {Rank}";
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		Color[] colors = (Color[])Enum.GetValues(typeof(Color));
+		Rank[] ranks = (Rank[])Enum.GetValues(typeof(Rank));
+
+		Card[] deck = new Card[colors.Length * ranks.Length];
+
+		for (int i = 0; i < colors.Length; i++)
+		{
+			for (int j = 0; j < ranks.Length; j++)
+			{
+				int index = i * ranks.Length + j;
+				deck[index] = new Card(colors[i], ranks[j]);
+			}
+		}
+
+		foreach (Card c in deck) 
+		{ 
+			Console.WriteLine(c); 
+		}
+	}
+}
---
+++ level_24_color/Program.cs
@@ -0,0 +1,45 @@
+﻿// The Color
+// - Define a new color class for traditional RGB channels.
+// - Add appropriate constructor
+// - Create factory props to define the 8 common colors.
+// - In main make two Colors, one with a constructor the other with a factory prop.
+
+class Color
+{
+	public byte R { get; }
+	public byte G { get; }
+	public byte B { get; }
+
+	public Color () {}
+
+	public Color (byte red, byte green, byte blue)
+	{
+		R = red;
+		G = green;
+		B = blue;
+	}
+
+	public static Color ColorRed()		=> new Color(255,0,0);
+	public static Color ColorGreen() 	=> new Color(0,255,0);
+	public static Color ColorBlue() 	=> new Color(0,0,255);
+	public static Color ColorYellow() 	=> new Color(255,255,0);
+	public static Color ColorCyan() 	=> new Color(0,255,255);
+	public static Color ColorMagenta() 	=> new Color(255,0,255);
+	public static Color ColorBlack() 	=> new Color(255,255,255);
+	public static Color ColorWhite() 	=> new Color(0,0,0);
+
+	public override string ToString() { return $"({R}, {G}, {B})"; }
+
+}
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		var color1 = new Color();
+		var color2 = new Color(123,10,80);
+		var color3 = Color.ColorRed();
+
+		Console.Write($"{color1}\n{color2}\n{color3}\n");
+	}
+}
